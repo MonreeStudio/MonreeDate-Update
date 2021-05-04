e9299be2fb8d1ddeba4b415cb62f7956fe9e34fe
MonreeStudio <monreestudio@outlook.com>
2021-05-04 22:09:46 +0800

    完成了注册、登录和注销流程的编写。

--- 倒计时/Models/ToDoTaskSteps.cs
+++ 倒计时/Models/ToDoTaskSteps.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace 倒计时.Models
 {
-    class ToDoTaskSteps
+    public class ToDoTaskSteps
     {
-        public int ParentId { get; set; }
+        public string TaskName { get; set; }
 
         public string Content { get; set; }
+
+        public bool Finish { get; set; }
+
+        public bool UnFinish { get; set; }
+    }
+
+    public class ToDoTaskStepsViewModel
+    {
+        public ObservableCollection<ToDoTaskSteps> ToDoTaskStepsDatas = new ObservableCollection<ToDoTaskSteps>();
+        public ToDoTaskStepsViewModel()
+        {
+            ToDoTaskStepsDatas.Add(new ToDoTaskSteps() { TaskName = "", Content = "一个小脚印", Finish = true });
+        }
     }
 }
